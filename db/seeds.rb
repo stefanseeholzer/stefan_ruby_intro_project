bfb48e8943743218e8c23deca573a73290df93e5
stefanseeholzer <stefanseeholzer99@gmail.com>
2024-02-12 18:52:52 -0600

    Added csv data and started coding seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,38 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'csv'
+
+Planet.delete_all
+Species.delete_all
+Character.delete_all
+
+filename = Rails.root.join("db/data/planets.csv")
+
+puts "Loading Planets: #{filename}"
+
+csv_data = File.read(filename)
+planets = CSV.parse(csv_data, headers: true, encoding: "utf-8")
+
+
+
+
+
+
+
+filename = Rails.root.join("db/data/species.csv")
+
+puts "Loading Species: #{filename}"
+
+csv_data = File.read(filename)
+planets = CSV.parse(csv_data, headers: true, encoding: "utf-8")
+
+
+
+
+
+
+
+filename = Rails.root.join("db/data/characters.csv")
+
+puts "Loading Characters: #{filename}"
+
+csv_data = File.read(filename)
+planets = CSV.parse(csv_data, headers: true, encoding: "utf-8")

f7d7156a9d7d97e608d6074d305da39613c098cb
stefanseeholzer <stefanseeholzer99@gmail.com>
2024-02-27 18:22:37 -0600

    Added links to switch between show pages

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -1,6 +1,6 @@
 class Planet < ApplicationRecord
     has_many :characters, foreign_key: 'homeworld', primary_key: 'planet_name'
-    has_many :species
+    has_many :species, foreign_key: 'homeworld', primary_key: 'planet_name'
 
     validates :planet_name, presence: true, uniqueness: true
     validates :diameter, :surface_water, numericality: true
--- app/models/species.rb
+++ app/models/species.rb
@@ -1,6 +1,6 @@
 class Species < ApplicationRecord
     belongs_to :planet, foreign_key: "homeworld", primary_key: "planet_name"
-    has_many :characters
+    has_many :characters, foreign_key: 'species', primary_key: 'species_name'
 
     validates :species_name, presence: true, uniqueness: true
     validates :average_height, numericality: true
